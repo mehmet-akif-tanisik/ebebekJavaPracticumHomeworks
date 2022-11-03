a625a38d0ba5c381daa097c9c21d9e2cc4749089
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:21:30 +0300

    ReversedTriangleStarShape.java added to week3

---
+++ src/week3/ReversedTriangleStarShape.java
@@ -0,0 +1,24 @@
+package week3;
+
+import java.util.Scanner;
+
+public class ReversedTriangleStarShape {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Total digit number: ");
+        int number = keyboard.nextInt();
+
+        for (int i = number; i >= 1; i--) {
+            for (int j = number - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i - 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
