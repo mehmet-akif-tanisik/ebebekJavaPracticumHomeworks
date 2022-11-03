7cd710924bb4213cb52497de793956511d9338cf
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:17:22 +0300

    ChineseZodiacSignFinder.java added to week2

---
+++ src/week2/ChineseZodiacSignFinder.java
@@ -0,0 +1,58 @@
+package week2;
+
+
+
+import java.util.Scanner;
+
+public class ChineseZodiacSignFinder {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a birth year: ");
+        int year = input.nextInt();
+
+        String zodiacSign = "";
+
+        switch (year % 12) {
+            case 0:
+                zodiacSign = "Monkey";
+                break;
+            case 1:
+                zodiacSign = "Rooster";
+                break;
+            case 2:
+                zodiacSign = "Dog";
+                break;
+            case 3:
+                zodiacSign = "Pig";
+                break;
+            case 4:
+                zodiacSign = "Rat";
+                break;
+            case 5:
+                zodiacSign = "Ox";
+                break;
+            case 6:
+                zodiacSign = "Tiger";
+                break;
+            case 7:
+                zodiacSign = "Rabbit";
+                break;
+            case 8:
+                zodiacSign = "Dragon";
+                break;
+            case 9:
+                zodiacSign = "Snake";
+                break;
+            case 10:
+                zodiacSign = "Horse";
+                break;
+            case 11:
+                zodiacSign = "Sheep";
+                break;
+        }
+
+        System.out.println("Your chinese zodiac sign is " + zodiacSign);
+    }
+}
