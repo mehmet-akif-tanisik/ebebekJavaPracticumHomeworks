0fbabfe9853cccab803bf0fae674d5db24e73b7a
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:18:39 +0300

    FibonacciSeries.java added to week3

---
+++ src/week3/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package week3;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter element number:");
+        int count = keyboard.nextInt();
+
+        int n1 = 0, n2 = 1, sum;
+
+        System.out.print(n1 + " " + n2);
+
+        for (int i = 2; i < count; ++i)
+        {
+            sum = n1 + n2;
+            System.out.print(" " + sum);
+            n1 = n2;
+            n2 = sum;
+        }
+
+    }
+}
