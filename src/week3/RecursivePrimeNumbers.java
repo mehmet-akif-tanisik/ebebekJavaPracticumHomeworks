10b0f2941d2fdcdc9e2c8ebbef4a2adc3b4ee192
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:21:18 +0300

    RecursivePrimeNumbers.java added to week3

---
+++ src/week3/RecursivePrimeNumbers.java
@@ -0,0 +1,28 @@
+package week3;
+
+import java.util.Scanner;
+
+public class RecursivePrimeNumbers {
+    static boolean isPrime(int n, int i)
+    {
+        if (n <= 2)
+            return n == 2;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+
+        return isPrime(n, i + 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter a number to see if it is Prime: ");
+        int number = keyboard.nextInt();
+
+        System.out.println(isPrime(number,2));
+
+    }
+}
