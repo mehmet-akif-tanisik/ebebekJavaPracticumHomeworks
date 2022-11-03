2a75021c7bfce1815508b7c84c23b44ec71fa7d8
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:19:56 +0300

    MinMaxFinder.java added to week3

---
+++ src/week3/MinMaxFinder.java
@@ -0,0 +1,26 @@
+package week3;
+
+import java.util.Scanner;
+
+public class MinMaxFinder {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("How many numbers you want to enter: ");
+        int n = keyboard.nextInt();
+
+        int max = 0, min = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(i + ". number: ");
+            int number = keyboard.nextInt();
+
+            max = Math.max(max, number);
+            min = Math.min(min, number);
+        }
+
+        System.out.println("Max number: " + max);
+        System.out.println("Min number: " + min);
+    }
+}
