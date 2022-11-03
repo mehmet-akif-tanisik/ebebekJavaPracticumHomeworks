dba00265a7849bc8d363d9ccbff79cec41f40e9d
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:21:02 +0300

    RecursivePowFinder.java added to week3

---
+++ src/week3/RecursivePowFinder.java
@@ -0,0 +1,27 @@
+package week3;
+
+import java.util.Scanner;
+
+public class RecursivePowFinder {
+
+    public static int power(int base, int pow) {
+        if (pow != 0) {
+            return (base * power(base, pow - 1));
+        }
+        else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter base: ");
+        int base = keyboard.nextInt();
+        System.out.print("Please enter pow: ");
+        int pow = keyboard.nextInt();
+
+        System.out.println(power(base,pow));
+    }
+}
