e77386a29e18a5b8027bab03abfdf444a766103c
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-04 01:19:23 +0300

    LeapYearFinder.java added to week2

---
+++ src/week2/LeapYearFinder.java
@@ -0,0 +1,18 @@
+package week2;
+
+import java.util.Scanner;
+
+public class LeapYearFinder {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter a year: ");
+        int year = keyboard.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            System.out.println(year + " is a leap year!");
+        else
+            System.out.println(year + " is not a leap year!");
+    }
+}
