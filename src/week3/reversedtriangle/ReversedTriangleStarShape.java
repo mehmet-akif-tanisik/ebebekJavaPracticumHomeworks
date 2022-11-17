075a60f707579e02129bb29d2517890878a41fdd
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 13:40:25 +0300

    ReversedTriangleStarShape.java updated

--- src/week3/reversedtriangle/ReversedTriangleStarShape.java
+++ src/week3/reversedtriangle/ReversedTriangleStarShape.java
@@ -1,16 +1,18 @@
 package week3.reversedtriangle;
-
 import java.util.Scanner;
 
 public class ReversedTriangleStarShape {
 
     public static void main(String[] args) {
+        System.out.println();
+        readInput();
+    }
 
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.print("Total digit number: ");
-        int number = keyboard.nextInt();
-
+    /**
+     * This method prints number n times stars and in every iteration stars number will be decreased one till zero.
+     * @param number is a depth of a triangle
+     */
+    public static void printPattern(int number){
         for (int i = number; i >= 1; i--) {
             for (int j = number - i; j >= 1; j--) {
                 System.out.print(" ");
@@ -20,5 +22,18 @@ public class ReversedTriangleStarShape {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * readInput() method gets number and calls printPattern() method.
+     * Outputs the triangle shape made with stars.
+     */
+    public static void readInput(){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Total digit number: ");
+        int number = keyboard.nextInt();
+
+        printPattern(number);
     }
 }
