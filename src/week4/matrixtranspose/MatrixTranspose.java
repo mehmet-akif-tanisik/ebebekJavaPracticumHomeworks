88661d2e9f4058917e971d276bac5d453a1939fe
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 14:31:04 +0300

    MatrixTranspose.java updated

--- src/week4/matrixtranspose/MatrixTranspose.java
+++ src/week4/matrixtranspose/MatrixTranspose.java
@@ -6,6 +6,11 @@ import java.util.Arrays;
 
 public class MatrixTranspose {
 
+    /**
+     * Takes transpose of an matrix parameter.
+     * @param matrix is an MxN size matrix.
+     * @return tranpose of MxN which is NxM.
+     */
     public static int[][] transpose(int[][] matrix){
         int[][] transpose = new int[matrix[0].length][matrix.length];
         for(int i=0 ; i< transpose.length; i++){
@@ -16,14 +21,20 @@ public class MatrixTranspose {
         return transpose;
     }
 
+    /**
+     * Prints entered matrix to the screen properly.
+     * @param arr given matrix MxN
+     */
     public static void printMatrix(int[][] arr){
         for (int[] element: arr){
             System.out.println(Arrays.toString(element));
         }
     }
 
+    /**
+     * Test method to test methods of an MatrixTranspose Class
+     */
     public static void main(String[] args) {
-
         int[][] matrix = { {1,2,3},{4,5,6}};
         int[][] matrix2 = {{4,9,12,14,17},{3,4,89,45,23}};
 
@@ -38,8 +49,5 @@ public class MatrixTranspose {
 
         System.out.println("Transpose of matrix:");
         printMatrix(transpose(matrix2));
-
-
     }
 }
-
