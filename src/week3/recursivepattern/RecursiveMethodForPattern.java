7b8ca697566e1d06c103775791be4ec12dc858cf
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 13:39:49 +0300

    RecursiveMethodForPattern.java updated

--- src/week3/recursivepattern/RecursiveMethodForPattern.java
+++ src/week3/recursivepattern/RecursiveMethodForPattern.java
@@ -1,8 +1,19 @@
 package week3.recursivepattern;
-
 import java.util.Scanner;
 
 public class RecursiveMethodForPattern {
+
+
+    public static void main(String[] args) {
+        readInput();
+    }
+
+    /**
+     * Recursive method to start with initial number n. Decreases the number by 5.
+     * If the number will be zero or negative, then increases the number till starting number again.
+     * Prints each numbers in the pattern to the screen.
+     * @param n is a starting number to create pattern
+     */
     public static void printNum(int n){
         System.out.print(n + " ");
         if (n<=0){
@@ -12,8 +23,11 @@ public class RecursiveMethodForPattern {
         System.out.print(n + " ");
     }
 
-    public static void main(String[] args) {
-
+    /**
+     * readInput() method gets starting number input and calls printNum() method.
+     * Prints special number pattern on the screen.
+     */
+    public static void readInput(){
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Please enter starting number: ");
