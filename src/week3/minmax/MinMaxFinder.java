5ae674c2e6323ca5664e861b21539b1fc2f8b0d6
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 13:39:10 +0300

    MinMaxFinder.java updated

--- src/week3/minmax/MinMaxFinder.java
+++ src/week3/minmax/MinMaxFinder.java
@@ -1,14 +1,18 @@
 package week3.minmax;
-
 import java.util.Scanner;
 
 public class MinMaxFinder {
     public static void main(String[] args)
     {
-        Scanner keyboard = new Scanner(System.in);
+        readInput();
+    }
 
-        System.out.print("How many numbers you want to enter: ");
-        int n = keyboard.nextInt();
+    /**
+     * Takes n numbers from user and prints out min and max of them.
+     * @param n is total number that user will enter
+     * @param keyboard object that will read user inputs
+     */
+    public static void findMinMax(int n, Scanner keyboard){
 
         int max = 0, min = 0;
 
@@ -22,5 +26,18 @@ public class MinMaxFinder {
 
         System.out.println("Max number: " + max);
         System.out.println("Min number: " + min);
+    }
+
+    /**
+     * readInput() method gets total number input which is a total number of decimal from user
+     * and calls findMinMax() method. Outputs min and max out of entered numbers as a String on the screen.
+     */
+    public static void readInput(){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("How many numbers you want to enter: ");
+        int n = keyboard.nextInt();
+
+        findMinMax(n, keyboard);
     }
 }
