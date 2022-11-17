b0631b954074546128e70881bb04c88e63d57dcc
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 13:40:02 +0300

    RecursivePowFinder.java updated

--- src/week3/recursivepow/RecursivePowFinder.java
+++ src/week3/recursivepow/RecursivePowFinder.java
@@ -1,20 +1,31 @@
 package week3.recursivepow;
-
 import java.util.Scanner;
 
 public class RecursivePowFinder {
+    public static void main(String[] args) {
+        readInput();
+    }
 
-    public static int power(int base, int pow) {
-        if (pow != 0) {
-            return (base * power(base, pow - 1));
+    /**
+     * Returns the value of the first argument raised to the power of the second argument.
+     * @param a the base
+     * @param b the exponent
+     * @return the value a to the power of b.
+     */
+    public static int power(int a, int b) {
+        if (b != 0) {
+            return (a * power(a, b - 1));
         }
         else {
             return 1;
         }
     }
 
-    public static void main(String[] args) {
-
+    /**
+     * readInput() method gets base and pow inputs from user
+     * and calls power() method. Outputs base to the power of pow as a String on the screen.
+     */
+    public static void readInput(){
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Please enter base: ");
@@ -22,6 +33,6 @@ public class RecursivePowFinder {
         System.out.print("Please enter pow: ");
         int pow = keyboard.nextInt();
 
-        System.out.println(power(base,pow));
+        System.out.println("Result: " + power(base,pow));
     }
 }
