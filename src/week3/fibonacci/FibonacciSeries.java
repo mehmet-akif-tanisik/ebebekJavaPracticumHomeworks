cb6b9a244dc201283dd934701c4562fc803b252f
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 13:38:52 +0300

    FibonacciSeries.java updated

--- src/week3/fibonacci/FibonacciSeries.java
+++ src/week3/fibonacci/FibonacciSeries.java
@@ -1,27 +1,41 @@
 package week3.fibonacci;
-
 import java.util.Scanner;
 
 public class FibonacciSeries {
 
     public static void main(String[] args)
     {
-        Scanner keyboard = new Scanner(System.in);
+        readInput();
+    }
 
-        System.out.print("Please enter element number:");
-        int count = keyboard.nextInt();
-
+    /**
+     * This method prints n element of Fibonacci series. (0-count)
+     * @param count total element of a Fibonacci series
+     */
+    public static void printFibonacciSeries(int count){
         int n1 = 0, n2 = 1, sum;
 
         System.out.print(n1 + " " + n2);
 
-        for (int i = 2; i < count; ++i)
+        for (int i = 2; i <= count; ++i)
         {
             sum = n1 + n2;
             System.out.print(" " + sum);
             n1 = n2;
             n2 = sum;
         }
+    }
 
+    /**
+     * readInput() method gets count input which is a total element number of a Fibonacci series from user
+     * and calls printFibonacciSeries() method. Outputs fibonacci numbers 0-count as a String on the screen.
+     */
+    public static void readInput(){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter element number:");
+        int count = keyboard.nextInt();
+
+        printFibonacciSeries(count);
     }
 }
