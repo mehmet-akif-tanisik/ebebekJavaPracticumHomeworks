25296638a94ef85172cb4c15a83d480b69ea8674
mehmet-akif-tanisik <matnsk@hotmail.com>
2022-11-17 13:39:22 +0300

    PerfectNumberFinder.java updated

--- src/week3/perfectnumber/PerfectNumberFinder.java
+++ src/week3/perfectnumber/PerfectNumberFinder.java
@@ -1,15 +1,19 @@
 package week3.perfectnumber;
-
 import java.util.Scanner;
 
 public class PerfectNumberFinder {
 
     public static void main(String[] args)
     {
-        Scanner keyboard = new Scanner(System.in);
+        readInput();
+    }
 
-        System.out.print("Please enter a number: ");
-        int val = keyboard.nextInt();
+    /**
+     * This method checks if the number is Perfect Number or not.
+     * @param val is user entered decimal value
+     * @return true if the val is a Perfect number. Otherwise, false.
+     */
+    public static boolean isPerfectNumber(int val){
 
         int sum = 0;
 
@@ -17,6 +21,19 @@ public class PerfectNumberFinder {
             if (val % i == 0)
                 sum += i;
         }
-        System.out.println(sum == val ? "Perfect Number!" : "Not a Perfect Number!");
+        return sum==val;
+    }
+
+    /**
+     * readInput() method gets val input and use it in isPerfectNumber() method to check
+     * if the val is Perfect number or not. Outputs condition of being Perfect Number as a String on the screen.
+     */
+    public static void readInput(){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter a number: ");
+        int val = keyboard.nextInt();
+
+        System.out.println(isPerfectNumber(val) ? "Perfect Number!" : "Not a Perfect Number!");
     }
 }
